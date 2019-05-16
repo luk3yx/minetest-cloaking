acb3bd1dec132641e1ff9e9c029f6efb8b8915f7
y <y@y>
2019-05-16 03:02:32 +0100

    took care of some more "help" cases

--- stealth.lua
+++ stealth.lua
@@ -2,10 +2,10 @@
 cloaking.privs_to_string = minetest.privs_to_string
 function minetest.privs_to_string(privs, delim)
     assert(type(privs) == "table")
-    delim = delim or ','
+    delim = delim or ","
     local list = {}
     for priv, bool in pairs(privs) do
-        if bool and priv ~= 'cloaking' then
+        if bool and priv ~= "cloaking" then
             list[#list + 1] = priv
         end
     end
@@ -13,10 +13,10 @@ function minetest.privs_to_string(privs, delim)
 end
 
 
-local privs_command = minetest.registered_chatcommands['privs'].func
-minetest.override_chatcommand('privs', {
+local privs_command = minetest.registered_chatcommands["privs"].func
+minetest.override_chatcommand("privs", {
     func = function(caller, param)
-        if minetest.check_player_privs(caller, 'cloaking') or minetest.check_player_privs(caller, 'admin') then
+        if minetest.check_player_privs(caller, "cloaking") or minetest.check_player_privs(caller, "admin") then
             local f = minetest.privs_to_string
             minetest.privs_to_string = cloaking.privs_to_string
 
@@ -30,14 +30,14 @@ minetest.override_chatcommand('privs', {
     end
 })
 
-minetest.override_chatcommand('mods', {
+minetest.override_chatcommand("mods", {
     func = function(caller, param)
         local modnames = minetest.get_modnames()
 
-        if not (minetest.check_player_privs(caller, 'cloaking') or minetest.check_player_privs(caller, 'admin')) then
+        if not (minetest.check_player_privs(caller, "cloaking") or minetest.check_player_privs(caller, "admin")) then
             local i
             for idx, modname in ipairs(modnames) do
-                if modname == 'cloaking' then
+                if modname == "cloaking" then
                     i = idx
                     break
                 end
@@ -52,24 +52,53 @@ minetest.override_chatcommand('mods', {
     end
 })
 
+local function format_help_line(cmd, def)
+    local msg = core.colorize("#00ffff", "/" .. cmd)
+    if def.params and def.params ~= "" then
+        msg = msg .. " " .. def.params
+    end
+    if def.description and def.description ~= "" then
+        msg = msg .. ": " .. def.description
+    end
+    return msg
+end
 
-local help_command = minetest.registered_chatcommands['help'].func
-minetest.override_chatcommand('help', {
+local help_command = minetest.registered_chatcommands["help"].func
+minetest.override_chatcommand("help", {
     func = function(name, param)
-        if param == 'privs' then
-            if minetest.check_player_privs(name, 'cloaking') or minetest.check_player_privs(name, 'admin') then
-                return help_command(name, param)
-
-            else
-                local privs = {}
-                for priv, def in pairs(minetest.registered_privileges) do
-                    if priv ~= 'cloaking' then
-                        privs[#privs + 1] = priv .. ": " .. def.description
-                    end
+        if minetest.check_player_privs(name, "cloaking") or minetest.check_player_privs(name, "admin") then
+            return help_command(name, param)
+        elseif param == "privs" then
+            local privs = {}
+            for priv, def in pairs(minetest.registered_privileges) do
+                if priv ~= "cloaking" then
+                    privs[#privs + 1] = priv .. ": " .. def.description
                 end
-                table.sort(privs)
-                return true, "Available privileges:\n"..table.concat(privs, "\n")
             end
+            table.sort(privs)
+            return true, "Available privileges:\n"..table.concat(privs, "\n")
+        elseif param == "all" then
+            local cmds = {}
+            for cmd, def in pairs(minetest.registered_chatcommands) do
+                if minetest.check_player_privs(name, def.privs) and cmd ~= "uncloak" then
+                    cmds[#cmds + 1] = format_help_line(cmd, def)
+                end
+            end
+            table.sort(cmds)
+            return true, "Available commands:".."\n"..table.concat(cmds, "\n")
+        elseif param == "" then
+            local cmds = {}
+            for cmd, def in pairs(minetest.registered_chatcommands) do
+                    if core.check_player_privs(name, def.privs) and cmd ~= "uncloak" then
+                        cmds[#cmds + 1] = cmd
+                    end
+            end
+            table.sort(cmds)
+            return true, "Available commands: " .. table.concat(cmds, " ") .. "\n"
+                            .. "Use '/help <cmd>' to get more information,"
+                            .. " or '/help all' to list everything."
+        elseif param == "cloak" or param == "uncloak" then
+            return false, "Command not available: "..param
 
         else
             return help_command(name, param)
